7479df1e06ac3a636610ea0ef940d3bb0100796c
blueskyxzy <18656432607@163.com>
2020-08-05 16:43:08 +0800

    add 网关 路由

---
+++ xframe-core/xframe-gateway/xframe-gateway-common/src/main/java/com/xzy/xframe/gateway/common/filter/RequestTimeFilter.java
@@ -0,0 +1,32 @@
+package com.xzy.xframe.gateway.common.filter;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.core.Ordered;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+public class RequestTimeFilter implements GatewayFilter, Ordered {
+    private static final Log log = LogFactory.getLog(GatewayFilter.class);
+    private static final String REQUEST_TIME_BEGIN = "requestTimeBegin";
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        exchange.getAttributes().put(REQUEST_TIME_BEGIN, System.currentTimeMillis());
+        return chain.filter(exchange).then(
+                Mono.fromRunnable(() -> {
+                    Long startTime = exchange.getAttribute(REQUEST_TIME_BEGIN);
+                    if (startTime != null) {
+                        log.info("请求路径：" + exchange.getRequest().getURI().getRawPath() + "消耗时间: " + (System.currentTimeMillis() - startTime) + "ms");
+                    }
+                })
+        );
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
